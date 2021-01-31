6afd630be4de815c03d4971851c260dc8c17bf67
Rye Miller <millerrye17@gmail.com>
2021-01-31 01:34:10 -0700

    Addition of the ask command for a placeholder.

---
+++ cmd/eddie/cmd/ask.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var askCmd = &cobra.Command{
+	Use: "ask [reminder]",
+	Short: "Reminders for tracking activity",
+	Long: `
+Eddie will set you a reminder for compiling this information.
+
+Among other things xD.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Set an example from eddie.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(askCmd)
+}
