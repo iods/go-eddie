80d6a1a25a5a74cce3fe64fe21573cc17ea6dad9
Rye Miller <millerrye17@gmail.com>
2021-01-31 01:34:48 -0700

    Addition of the report, track commands for placeholders.

---
+++ cmd/eddie/cmd/report.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+var reportCmd = &cobra.Command{
+	Use: "report [behavior]",
+	Short: "Generate reports for an activity",
+	Long: `
+Eddie will get a report to you.
+
+Among other things xD. Once Eddie generates all the data,
+he creates some dashboards, then builds a webpage with some
+cool looking data about what you have been up to.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Eddie will show you what your " + strings.Join(args, "") + " looks like lately.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(reportCmd)
+}
