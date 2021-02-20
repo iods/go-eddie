5a4ad1b152c275f4121b7027425968c166d496c0
Rye Miller <millerrye17@gmail.com>
2021-02-19 18:52:29 -0700

    Update to structs location to eliminate confusion for database models.

--- internal/db/schema/record.go
+++ internal/db/schema/record.go
@@ -1,1 +1,27 @@
 package schema
+
+import "gorm.io/gorm"
+
+type Record struct {
+	gorm.Model
+
+	Length    uint    `json:"length"`
+	Emojis	  []Emoji `gorm:"many2many:emojis;" json:"emojis"`
+	Frequency uint    `json:"frequency"`
+	From      string  `json:"from"`
+	Important bool    `json:"important"`
+	Location  string  `json:"location"`
+	Quality   uint    `json:"quality"`
+	Tags      []Tag   `gorm:"many2many:tags;" json:"tags"`
+	To        string  `json:"to"`
+	Total     string  `json:"total"`
+	Type      string  `json:"type"`
+}
+
+type Emoji struct {
+	Name string `gorm:"primary_key" json:"emoji_name"`
+}
+
+type Tag struct {
+	Name string `gorm:"primary_key" json:"tag_name"`
+}
