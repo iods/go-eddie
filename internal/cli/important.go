05fb2c25ca6363e2dc75014f61837fa2e0a51ca9
Rye Miller <millerrye17@gmail.com>
2021-02-20 21:00:24 -0700

    Update to the important flag for usage in later filters.

--- internal/cli/important.go
+++ internal/cli/important.go
@@ -3,6 +3,8 @@ package cli
 import "fmt"
 
 func isImportant(b bool) {
+	// will do something to these
+	// SELECT stmt pulling all important ones maybe, then filter then...
 	if b != false {
 		fmt.Println("This record was important")
 	}
