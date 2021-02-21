2e382339ff1084a0e85ed8bf795ac6ad3a816320
Rye Miller <millerrye17@gmail.com>
2021-02-20 21:20:35 -0700

    Update docs for import details.

--- internal/cli/important.go
+++ internal/cli/important.go
@@ -2,9 +2,11 @@ package cli
 
 import "fmt"
 
+// isImportant Tags records that are of importance in one way or another.
 func isImportant(b bool) {
 	// will do something to these
 	// SELECT stmt pulling all important ones maybe, then filter then...
+	// --important, -i flag
 	if b != false {
 		fmt.Println("This record was important")
 	}
