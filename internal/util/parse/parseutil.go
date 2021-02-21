2a32f42574bd47e74e4f703d5b223808b389fc3e
Rye Miller <millerrye17@gmail.com>
2021-02-20 20:57:10 -0700

    Addition of parseutil for tags and emoji slices.

---
+++ internal/util/parse/parseutil.go
@@ -0,0 +1,27 @@
+package parse
+
+import "github.com/iods/go-eddie/internal/db/schema"
+
+func Emojis(e []string) []schema.Emoji {
+	var emojis []schema.Emoji
+
+	l := len(e)
+	for i := 0; i < l; i++ {
+		emoji := []schema.Emoji{{Name: e[i]}}
+		emojis = append(emojis, emoji...)
+	}
+
+	return emojis
+}
+
+func Tags(t []string) []schema.Tag {
+	var tags []schema.Tag
+
+	l := len(t)
+	for i := 0; i < l; i++ {
+		tag := []schema.Tag{{Name: t[i]}}
+		tags = append(tags, tag...)
+	}
+
+	return tags
+}
