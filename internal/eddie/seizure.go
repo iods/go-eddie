526be75ce234694389274267e4c194884cbf0bf0
Rye Miller <millerrye17@gmail.com>
2021-02-28 01:55:03 -0700

    Removal of some output on create for cleanup and refactor.

--- internal/eddie/seizure.go
+++ internal/eddie/seizure.go
@@ -1,35 +1,25 @@
-package cli
+package eddie
 
 import (
-	"fmt"
-
 	"github.com/iods/go-eddie/internal/db"
 	"github.com/iods/go-eddie/internal/db/schema"
 	"github.com/iods/go-eddie/internal/helpers/time"
 	"github.com/iods/go-eddie/internal/util/parse"
 )
 
-// TrackSeizure
-func TrackSeizure(s string, tags []string, l string, b bool) (err error) {
+// TrackSeizure Structures and creates a record for tracking a seizure.
+func TrackSeizure(s string, tags []string, l string, isImportant bool) (err error) {
 	db.InitDatabase()
 	database := db.GetDatabase()
 
 	t := parse.Tags(tags)
-
 	r := &schema.Record{
 		Type: "seizure",
 		Time: time.UpdateTime(s),
 		Tags: t,
 		Location: l,
-		Important: b,
+		Important: isImportant,
 	}
-
 	database.Create(&r)
-
-	fmt.Printf("You recorded a seizure at %v\n", r.Time)
-	fmt.Println("Record ID:", r.ID)
-	fmt.Println("Tags:", r.Tags)
-	fmt.Println("Location:", r.Location)
-	isImportant(b)
 	return err
 }
