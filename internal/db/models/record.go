5be75e72ceaf107bc3b3a07f5230f2b6b224345a
Rye Miller <millerrye17@gmail.com>
2021-02-28 22:58:40 -0700

    Update to some methods for the RecordModel accessing with conditions.

--- internal/db/models/record.go
+++ internal/db/models/record.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/iods/go-eddie/internal/db"
@@ -11,9 +12,11 @@ import (
 type RecordModel struct {}
 
 var (
-	format = "2006-01-02"
+	format  = "2006-01-02"
+	result  schema.Record
 	results []schema.Record
 )
+
 
 // GetRecords Returns all entities from the datasource for ...
 func (r RecordModel) GetRecords() ([]schema.Record, error) {
@@ -24,53 +27,103 @@ func (r RecordModel) GetRecords() ([]schema.Record, error) {
 	return results, nil
 }
 
-// GetRecordsFirst Returns the first created_at record in the database for use with filtering and calculations.
-func (r RecordModel) GetRecordsFirst() (schema.Record, error) {
+
+// GetRecordsBetween Returns a slice of records based on a record type, a field, and a range.
+func (r RecordModel) GetRecordsBetween(recordType string, flag string, min float64, max float64) ([]schema.Record, error) {
 	db.InitDatabase()
 	database := db.GetDatabase()
 
-	var result schema.Record
-	database.Debug().Order("created_at asc").First(&result)
-	return result, nil
-}
-
-// GetRecordsLast Returns the most recent created_at record in the database for use with filtering and calculations.
-func (r RecordModel) GetRecordsLast() (schema.Record, error) {
-	db.InitDatabase()
-	database := db.GetDatabase()
-
-	var result schema.Record
-	database.Debug().Order("created_at desc").First(&result)
-	return result, nil
-}
-
-// GetRecordsByDate Extends GetRecords by returning records based on a specified date for ...
-func (r RecordModel) GetRecordsByDate(d string) ([]schema.Record, error) {
-	db.InitDatabase()
-	database := db.GetDatabase()
-
-	q, _ := time.Parse(format, d)
-	database.Debug().Where("created_at = ?", q).Find(&results)
+	query := fmt.Sprintf("%s BETWEEN %f AND %f", flag, min, max)
+	database.Debug().Where("type = ?", recordType).Where(query).Find(&results)
 	return results, nil
 }
 
+
+// GetRecordsOfType Returns records of a specified type for filtering and extending GetRecords.
+func (r RecordModel) GetRecordsOfType(recordType string) ([]schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	database.Debug().Where("type = ?", recordType).Find(&results)
+	return results, nil
+}
+
+
+// GetRecordOfType Returns a single record (first or last created_at) of the type specified.
+func (r RecordModel) GetRecordOfType(recordType string, desc bool) (schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	var value string
+	if desc == false {
+		value = "created_at asc"
+	} else {
+		value = "created_at"
+	}
+	database.Debug().Where("type = ?", recordType).Order(value).First(&result)
+	return result, nil
+}
+
+
+// GetRecordsByDate Extends GetRecords by returning records based on a specified date for ...
+func (r RecordModel) GetRecordsByDate(d string, recordType string) ([]schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	query, _ := time.Parse(format, d)
+	database.Debug().Where("created_at = ? AND type = ?", query, recordType).Find(&results)
+	return results, nil
+}
+
+
 // GetRecordsByDateRange Returns a set of records created between a provided date range for ...
-func (r RecordModel) GetRecordsByDateRange(from string, to string) ([]schema.Record, error) {
+func (r RecordModel) GetRecordsByDateRange(from string, to string, recordType string) ([]schema.Record, error) {
 	db.InitDatabase()
 	database := db.GetDatabase()
 
 	f, err := time.Parse(format, from)
 	t, err := time.Parse(format, to)
 	errors.Handle("issue", err)
-	database.Debug().Where("created_at BETWEEN ? AND ?", f, t).Find(&results)
+	database.Debug().Where("created_at BETWEEN ? AND ?", f, t).Where("type = ?", recordType).Find(&results)
 	return results, nil
 }
 
-// GetRecordsByImportance Returns a slice of records based on their importance, true or false.
-func (r RecordModel) GetRecordsByImportance() ([]schema.Record, error) {
+
+// GetRecordsStartingWith Returns a slice of related records.
+func (r RecordModel) GetRecordsStartingWith(keyword string, flag string) ([]schema.Record, error) {
 	db.InitDatabase()
 	database := db.GetDatabase()
 
-	database.Debug().Find(&results, "important = ?", true)
+	database.Debug().Where(flag + " LIKE ?", keyword + "%").Find(&results)
+	return results, nil
+}
+
+
+// GetRecordsStartingWith Returns a slice of related records.
+func (r RecordModel) GetRecordsContaining(keyword string, flag string) ([]schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	database.Debug().Where(flag + " LIKE ?", "%" + keyword + "%").Find(&results)
+	return results, nil
+}
+
+
+// GetRecordsStartingWith Returns a slice of related records.
+func (r RecordModel) GetRecordsEndingWith(keyword string, flag string) ([]schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	database.Debug().Where(flag + " LIKE ?", "%" + keyword).Find(&results)
+	return results, nil
+}
+
+
+// GetRecordsByImportance Returns a slice of records based on their importance, true or false.
+func (r RecordModel) GetRecordsByImportance(recordType string) ([]schema.Record, error) {
+	db.InitDatabase()
+	database := db.GetDatabase()
+
+	database.Debug().Find(&results, "type = ? AND important = true", recordType)
 	return results, nil
 }
