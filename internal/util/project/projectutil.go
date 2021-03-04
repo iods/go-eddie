21199eae95eeb20da055b2b77b712967990211f6
Rye Miller <millerrye17@gmail.com>
2021-03-04 05:24:09 -0700

    Update of init flow and addition of promptui for installation questions.

--- internal/util/project/projectutil.go
+++ internal/util/project/projectutil.go
@@ -2,11 +2,14 @@ package util
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 
 	"github.com/iods/go-eddie/internal/errors"
+	"github.com/manifoldco/promptui"
 )
 
 type Project struct {
@@ -29,14 +32,73 @@ func loadConfig(dir string)  {
 	filename := filepath.Join(dir, "/config")
 	_, err := os.Stat(filename)
 	if os.IsNotExist(err) {
-		if _, err := os.Stat(filename); os.IsNotExist(err) {
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
 			err = os.MkdirAll(dir, os.ModePerm)
 		}
-		f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		errors.Handle("handle this error at some point", err)
-		f.WriteString("eddie, the cli service dog\n\n")
-		f.Close()
+		_, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0644)
+		errors.Handle("unable to open file", err)
 	}
+}
+
+func loadPrompt() bool {
+	validate := func(input string) error {
+		_, err := strconv.ParseFloat(input, 64)
+		return err
+	}
+
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{  . }}\n\n",
+		Valid:   "{{  . | bold | green }} ",
+		Invalid: "{{  . | bold}} ",
+		Success: "{{  . | bold | green }} ",
+
+	}
+
+	fmt.Printf(`
+ so you've decided to take home
+   _______ ______  ______  _____ _______
+   |______ |     \ |     \   |   |______
+   |______ |_____/ |_____/ __|__ |______
+
+ there are a few things eddie needs to know
+ before you can get started:
+
+`)
+	sp := promptui.Prompt{
+		Label: " [1]  Would you like to stub the database? ",
+		Templates: templates,
+		IsConfirm: true,
+	}
+	one, err := sp.Run()
+
+	wp := promptui.Prompt{
+		Label: " [2]  What is your starting weight (lbs) ? ",
+		Templates: templates,
+		Validate: validate,
+	}
+	two, err := wp.Run()
+
+	hp := promptui.Prompt{
+		Label: " [3]  What is your starting height (in) ? ",
+		Templates: templates,
+		Validate: validate,
+	}
+	three, err := hp.Run()
+
+	if err != nil {
+		fmt.Printf("prompt failed %v\n", err)
+	}
+
+	writeConfig("weight", two)
+	writeConfig("height", three)
+
+	if one == "y" {
+		fmt.Printf("\nThat's it. Now run `eddie --help` to get started and view a demo setup.\n")
+		return true
+	}
+
+	fmt.Printf("\nThat's it. Now run `eddie --help` to get started.\n")
+	return false
 }
 
 // loadRecords Creates a records file for the application to write tracked records to.
@@ -55,6 +117,20 @@ func loadRecords(dir string, stub bool) {
 		}
 		_, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0644)
 		errors.Handle("unable to open file", err)
+	}
+}
+
+// writeConfig Writes the prompt answers to a configuration file for setting application defaults.
+func writeConfig(config string, record string) {
+	path := loadProject()
+	filename := filepath.Join(path.Directory, "/config")
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	errors.Handle("handle this some time.", err)
+	if _, err := f.Write([]byte(config + ":" + record + "\n")); err != nil {
+		f.Close()
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -78,6 +154,10 @@ func (p Project) CheckDirectory() bool {
 func (p Project) InstallDirectory() bool {
 	proj := loadProject()
 	loadConfig(proj.Directory)
-	loadRecords(proj.Directory, false)
+	stub := loadPrompt()
+	loadRecords(proj.Directory, stub)
+	if stub == false {
+		return false
+	}
 	return true
 }
